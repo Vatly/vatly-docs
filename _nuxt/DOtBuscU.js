86e79d0512706d6a74beda3db78de8466aaa331e
Sander van Hooft <sandervanhooft@users.noreply.github.com>
2026-05-19 21:41:03 +0000

    Updates

---
+++ _nuxt/DOtBuscU.js
@@ -0,0 +1,1 @@
+import{r}from"./BK-XGZL3.js";import t from"./Ifnq6imG.js";import{$ as a,a as e,p,s as c,o as n}from"./6Ffyb0f3.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(i){const o=a();return(s,f)=>(n(),e(t,{color:"success",icon:c(o).ui.icons.tip},{default:p(()=>[r(s.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
